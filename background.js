fd7d5f42bdaf04bf256862b1cb1e96caab7c7841
Chris Buteau <cbuteau@gmail.com>
2022-10-16 10:36:11 -0400

    minor tweaks

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ async function execScript() {
   const tabId = await getTabId();
   chrome.scripting.executeScript({
     target: {tabId: tabId},
-    files: ['execute.js']
+    files: ['src/execute.js']
   })
 }
 
@@ -13,3 +13,16 @@ async function getTabId() {
   const tabs = await chrome.tabs.query({active: true, currentWindow: true});
   return (tabs.length > 0) ? tabs[0].id : null;
 }
+
+var executeCapLockClosure = function() {
+  console.log('CAPS LOCK YO');
+};
+
+var executeCapLockNoClosure = function() {
+  console.log('CAPS LOCK NO');
+};
+
+
+// document.addEventListener('keyup', function(event){
+//   event.getModifierState('CapsLock') ? executeCapLockClosure() : executeCapLockNoClosure();
+// });
